b5647bb38b738d3a09c3e6e15335d29669d0d684
xiejun199409 <slxwgxj@gmail.com>
2017-10-12 13:07:09 +0800

    #2 done

--- 2.go
+++ 2.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+)
+
+func fibo(x int) int{
+	if x == 1 {
+		return 1
+	} else if x== 2 {
+		return 2
+	} else {
+		return fibo(x-1) + fibo(x-2)
+	}
+}
+
+func main(){
+	var sum,j int
+	sum = 0
+	j = fibo(1)
+	for i := 2; j<4000000;i++ {
+		if j%2 == 0{
+			sum += j
+		}
+		j = fibo(i)
+	}
+	fmt.Println(sum)
+}
+
--- 3.go
+++ 3.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main(){
+	poolsize := int64(math.Sqrt(600851475143))
+	var primepool[]int64
+}
