14717cb26e9bd6f0cbaba913c93fa930fbaf8032
xiejun199409 <slxwgxj@gmail.com>
2017-10-12 15:03:55 +0800

    #6 done

--- 5.go
+++ 5.go
@@ -1,3 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
 
+func main(){
+	start := time.Now()
+	var result int
+	for result = 1 ;;result++{
+		for i := 2 ; i <=20 ; i++{
+			if result%i != 0 {
+				break
+			}
+			if i == 20 {
+				fmt.Println("Result is",result,"\nRunning time is",time.Since(start));
+				return
+			}
+		}
+	}
+}
--- 6.go
+++ 6.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	var squareSum,sumSquare,sum,result int
+	squareSum = 0
+	sum = 0
+	for i := 1 ; i <= 100 ; i++{
+		squareSum += i*i
+	}
+	for j := 1 ; j <= 100 ; j++{
+		sum += j
+	}
+	sumSquare = sum * sum
+	if sumSquare > squareSum {
+		result = sumSquare - squareSum
+	}else{
+		result = squareSum - sumSquare
+	}
+	fmt.Println("Result is", result, "\nRunning time is", time.Since(start));
+}
