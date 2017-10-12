d4ddb9bff421b9015c2c1d2f91ac262a1d76d71b
xiejun199409 <slxwgxj@gmail.com>
2017-10-12 19:00:19 +0800

    #9 done

--- 9.go
+++ 9.go
@@ -7,11 +7,13 @@ import (
 
 func main() {
 	start := time.Now()
-	for i := 1 ; i < 33 ; i++{
-		for j := i+1 ; i + 2 * j < 100 ; j++{
-			k := 100 - i - j
-			if i * i + j * j == k * k
+	for i := 1 ; i < 333 ; i++{
+		for j := i+1 ; i + 2 * j < 1000 ; j++{
+			k := 1000 - i - j
+			if (i * i + j * j) == k * k {
+				fmt.Println("Result is", i * j * k , "\nRunning time is", time.Since(start));
+				return
+			}
 		}
 	}
-	fmt.Println("Result is", num, "\nRunning time is", time.Since(start));
 }
