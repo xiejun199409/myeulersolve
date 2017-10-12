f8477d43a8f7b68908fc0901d4bf93a529feac85
xiejun199409 <slxwgxj@gmail.com>
2017-10-12 13:45:49 +0800

    #3 done

--- 3.go
+++ 3.go
@@ -3,9 +3,27 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"time"
 )
 
 func main(){
-	poolsize := int64(math.Sqrt(600851475143))
-	var primepool[]int64
+	var num,i int64
+	start := time.Now()
+	num = 600851475143
+	poolsize := int64(math.Sqrt(float64(num)))
+	for i = 2 ; i <= poolsize ; i++{
+		for true {
+			if (num%i == 0){
+				num /= i
+			}else{
+				if num == 1{
+					num = i
+				}
+				break
+			}
+		}
+	}
+	fmt.Println("Result is",num,"\nRunning time is",time.Since(start));
+
 }
