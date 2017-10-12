335a919c9f87dfa0389cb130ef9b30c6e74485cf
xiejun199409 <slxwgxj@gmail.com>
2017-10-12 14:39:11 +0800

    #4 done

--- 4.go
+++ 4.go
@@ -1,1 +1,35 @@
-package main
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func numcheck(x int) bool{
+	str := fmt.Sprintf("%d",x)
+	num := []byte(str)
+	for i := 0; i < len(num) ; i++ {
+		if num[i] != num[len(num)-i-1]{
+			return false
+		}
+	}
+	return true
+}
+
+func main(){
+	start := time.Now()
+	var num1,num2,sum int
+	last := 0
+	for num1 = 999 ; num1 > 99 ; num1--{
+		for num2 = 999;	num2 > 99 ; num2-- {
+			sum = num1 * num2
+			if numcheck(sum) == true{
+				if last < sum {
+					last = sum
+				}
+			}
+
+		}
+	}
+	fmt.Println("Result is",last,"\nRunning time is",time.Since(start));
+}
